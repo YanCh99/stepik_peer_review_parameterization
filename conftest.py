38fd9f96782d710035190a4db9aa2044f55952dc
Yan <yanchirv@gmail.com>
2026-09-18 13:57:08 +0300

    поменял название файла conftest и дописал в него фикстуру +логику перевдачи языка

---
+++ conftest.py
@@ -0,0 +1,28 @@
+
+# Реализовать логику запуска браузера с указанным языком пользователя.
+# Добавить фикстуру, которая открывает браузер и передается в тест как параметр.
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+# Обработчик, который считает из командной строки параметр language
+def pytest_addoption(parser):
+    parser.addoption(
+        "--language",
+        action="store",
+        default="en",
+        help="choose language: ru, en, es, etc."
+    )
+# Реализовать логику запуска браузера с указанным языком пользователя.
+# Добавить фикстуру, которая открывает браузер и передается в тест как параметр.
+@pytest.fixture(scope="function")
+def browser(request):
+    user_language = request.config.getoption("language")
+    options = Options()
+    options.add_axperimental_option('prefs', {'intl.accept_languages': user_language})
+    print("\nstart chrome browser for test..")
+    browser = webdriver.Chrome(options=options)
+    
+    yield browser
+    print("/nquit browser..")
+    browser.quit()
